3b3e16a8d19756d740fd221ced3f6297a2ba23a9
MalakMelhem <malak.melhem98@gmail.com>
2022-11-01 21:13:45 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        // autoplay:true,
+        // autoplayhoverpause:true,
+        // autoplaytimeout:100,
+        nav:true,
+        loop:true,
+        items:3
+
+    });
+//     let owlPrev =document.getElementsByClassName('owl-prev')[0];
+//     let owlNext =document.getElementsByClassName('owl-next')[0];
+
+//     function disable1(){
+//         this.classList.add("disable");
+//     }
+    
+// owlPrev.addEventListener("click",disable1);
+// owlNext.addEventListener("click",disable1);
+
+})
+
